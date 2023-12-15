b46bfc9d6cb0aa53be6be9c1deb3eac17ecf3e80
Sumon Chandra <topu199041@gmail.com>
2023-12-15 23:36:46 +0600

    chore: navbar completed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { Navbar as NextNavbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu } from "@nextui-org/react";
 import DesktopNavItem from "./DesktopNavItem";
 import MobileNavItem from "./MobileNavItem";
-import NavMenuIcon from "./NavMenuIcon";
+import { MdMenu } from "react-icons/md";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
 
 const Navbar = () => {
    const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -13,23 +15,29 @@ const Navbar = () => {
          isMenuOpen={isMenuOpen}
          onMenuOpenChange={setIsMenuOpen}
       >
-         <NavbarMenuToggle
-            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-            className="sm:hidden"
-         />
-         <NavbarBrand>
-            <NavMenuIcon />
-            <p className="font-bold text-inherit">ACME</p>
-         </NavbarBrand>
-         <NavbarContent className="hidden sm:flex gap-4" justify="start">
+         <NavbarContent className="sm:hidden" justify="start">
+            <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} icon={MdMenu} />
+         </NavbarContent>
+
+         <NavbarContent className="sm:hidden" justify="center">
+            <NavbarBrand className="select-none text-primary-color cursor-pointer ">
+               <h3 className="font-bold text-inherit">MC collage</h3>
+            </NavbarBrand>
+         </NavbarContent>
+
+         <NavbarContent className="hidden sm:flex" justify="start">
+            <NavbarBrand className="flex-grow-0 mr-6 select-none text-primary-color cursor-pointer">
+               <h3 className="font-bold text-inherit text-lg">MC collage</h3>
+            </NavbarBrand>
             <DesktopNavItem to="#" label="Home" />
             <DesktopNavItem to="#" label="Classes" />
             <DesktopNavItem to="#" label="Admission" />
             <DesktopNavItem to="#" label="My Classes" />
          </NavbarContent>
          <NavbarContent justify="end">
-            <NavbarItem>
-               Sign Up
+            <NavbarItem className="group relative transition-all flex items-center justify-start gap-2 cursor-pointer">
+               <div>Sign up</div>
+               <IoIosArrowRoundForward size={22} className="group-hover:translate-x-1 duration-300 " />
             </NavbarItem>
          </NavbarContent>
          <NavbarMenu>
